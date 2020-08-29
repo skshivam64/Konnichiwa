bff3c916953eea0d5135f6698be814ec3df17448
skshivam64 <engineeraccidental@gmail.com>
2020-08-29 18:49:38 +0530

    New UI for user authentication

--- res/js/auth.js
+++ res/js/auth.js
@@ -1,5 +1,32 @@
 $(document).ready(() => {
 
+  const loginBtn = document.getElementById('login');
+  const signupBtn = document.getElementById('signup');
+
+  loginBtn.addEventListener('click', (e) => {
+    let parent = e.target.parentNode.parentNode;
+    Array.from(e.target.parentNode.parentNode.classList).find((element) => {
+      if(element !== "slide-up") {
+        parent.classList.add('slide-up')
+      }else{
+        signupBtn.parentNode.classList.add('slide-up')
+        parent.classList.remove('slide-up')
+      }
+    });
+  });
+
+  signupBtn.addEventListener('click', (e) => {
+    let parent = e.target.parentNode;
+    Array.from(e.target.parentNode.classList).find((element) => {
+      if(element !== "slide-up") {
+        parent.classList.add('slide-up')
+      }else{
+        loginBtn.parentNode.parentNode.classList.add('slide-up')
+        parent.classList.remove('slide-up')
+      }
+    });
+  });
+
   $('.toggle').on('click', function() {
     $('.container').stop().addClass('active');
   });
@@ -21,7 +48,9 @@ $(document).ready(() => {
           $("#main-error").show()
           $("#error-container").text(data.error)
         }
-        window.location.href = '/'
+        else{
+          window.location.href = '/'
+        }
       }
     })
   })
